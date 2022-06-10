02950ce54da06214db63bdc4c114190934ec1543
Victor Souza <victorhugochimenton@icloud.com>
2022-06-10 20:22:24 -0300

    Implemented canRankup method 🐷

--- src/main/java/com/github/v1ctu/command/RankupCommand.java
+++ src/main/java/com/github/v1ctu/command/RankupCommand.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import me.saiintbrisson.minecraft.command.annotation.Command;
 import me.saiintbrisson.minecraft.command.command.Context;
 import me.saiintbrisson.minecraft.command.target.CommandTarget;
-import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -20,22 +19,15 @@ public class RankupCommand {
     public void handleCommand(Context<Player> context) {
         Player player = context.getSender();
         UserEntity userEntity = rankupPlugin.getUserEntityCache().get(player.getUniqueId());
-        if (userEntity == null) return;
+        if (userEntity == null || !userEntity.canRankup()) {
+            player.sendMessage("§cVoce nao pode upar de rank!");
+            return;
+        }
 
         Rank currentRank = userEntity.getRank();
         Rank nextRank = rankupPlugin.getRankCache().get(rank -> rank.getPosition() == (currentRank.getPosition() + 1));
-        if (nextRank == null) {
-            player.sendMessage("§cJá acabou filho esquece.");
-            return;
-        }
 
-        Economy economy = rankupPlugin.getEconomy();
-        if (economy.getBalance(player) < nextRank.getPrice()) {
-            player.sendMessage("§cVocê não tem dinheiro, pobre.");
-            return;
-        }
-
-        economy.withdrawPlayer(Bukkit.getOfflinePlayer(player.getUniqueId()), nextRank.getPrice());
+        rankupPlugin.getEconomy().withdrawPlayer(Bukkit.getOfflinePlayer(player.getUniqueId()), nextRank.getPrice());
         userEntity.setRank(nextRank);
 
         player.sendMessage("§aVocê evoluiu para o rank " + nextRank.getName() + "§a.");
--- src/main/java/com/github/v1ctu/entity/UserEntity.java
+++ src/main/java/com/github/v1ctu/entity/UserEntity.java
@@ -1,9 +1,11 @@
 package com.github.v1ctu.entity;
 
+import com.github.v1ctu.RankupPlugin;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import net.milkbowl.vault.economy.Economy;
 import org.bson.codecs.pojo.annotations.BsonIgnore;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -16,6 +18,8 @@ import java.util.UUID;
 @Builder
 @Data
 public class UserEntity {
+
+    private static final RankupPlugin rankupPlugin = RankupPlugin.getInstance();
 
     private UUID uuid;
     private Rank rank;
@@ -34,5 +38,12 @@ public class UserEntity {
         Player player = getPlayer();
         OfflinePlayer offlinePlayer = getOfflinePlayer();
         return (player == null) ? offlinePlayer.getName() : player.getName();
+    }
+
+    public boolean canRankup() {
+        Rank nextRank = rankupPlugin.getRankCache().get(cachedRank -> cachedRank.getPosition() == (rank.getPosition() + 1));
+        if (nextRank == null) return false;
+
+        return (rankupPlugin.getEconomy().getBalance(getPlayer()) < nextRank.getPrice());
     }
 }
